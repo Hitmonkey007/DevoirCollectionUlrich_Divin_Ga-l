2c7589fa78a7fc92382da04d2a82416c9f8fee6c
hitmonkey007 <igiranezaulrich19@gmail.com>
2023-05-11 19:03:27 +0200

    Fin de la Question I

---
+++ main.py
@@ -0,0 +1,87 @@
+#__Q.1__#
+
+##__Creation de la liste__##
+liste_vehicules = ['1. Kawasaki', '2. Bugati','3. Toyota','4. Subaru','5. BMW','6. Fiat','7. Chevrolet','8. Mercedes','9. Yamaha','10. Hyundai']
+print(' Question I  :')
+print('--------------')
+#--1-- Affichage des elements de la liste--#
+print("--1: La liste des Vehicules:")
+print('___________________________')
+for element in liste_vehicules:
+    print(element)
+
+#--2-- Changer l'element numero 5--#
+print("--2: Liste apres le changement du numero 5:")
+print('___________________________________________')
+liste_vehicules.insert (5,'5. Lamborghini')
+for element in liste_vehicules:
+    print(element)
+
+#--3-- Creer une nouvelle liste en remplissant avec les elements precedent--#
+print("--3: NOuvelle liste:")
+print('____________________')
+liste_new = []
+for elt in liste_vehicules:
+    if 'a' in elt:
+        liste_new.append('1. Tesla')
+for i in liste_new:
+    print(i)
+
+#--4-- Ajouter un element a la fin de la liste--#
+print('--4: Ajout element a la fin de la liste:')
+print('________________________________________')
+liste_vehicules.insert(11,'11. Ferrari') 
+for i in liste_vehicules:
+    print(i)
+
+#--5-- Ajouter un element a l'index numero 2--#
+print('--5: Ajout_element a l_index numero 2: ')
+print('_______________________________________')
+liste_vehicules.insert(2,'3. Jaguar')
+for j in liste_vehicules:
+    print(j)
+#--6-- Supprimer l'élément numéro 3--#
+print('--6: La suppression du vehicule numero 3 :')
+print('__________________________________________')
+del liste_vehicules[3]
+for i in liste_vehicules:
+    print(i)
+
+#--7-- Supprimer l'élément à l’index numéro 2--#
+print('--7: La suppresion du vehicule a l_index numero 2:')
+print('__________________________________________________') 
+del liste_vehicules[2]
+for i in liste_vehicules:
+    print(i)
+#--8--  Ordonner la liste --#
+print('--8: Liste ordonnee:')
+print('___________________') 
+liste_vehicules.sort()
+for i in liste_vehicules:
+    print(i)
+
+#--9-- Afficher la liste au sens inverse --#
+print('--9: Liste affichee en sens inverse:')
+print('____________________________________') 
+liste_vehicules.reverse()
+for i in liste_vehicules:
+    print(i)
+
+#--10-- Vider la liste --#
+print('--10: Vider la liste:')
+print('_____________________')
+liste_vehicules.clear()
+for j in liste_vehicules:
+    print(j)
+
+#--11-- Supprimer la liste --# 
+print('--11: La liste a ete supprimee')
+print('______________________________')
+del liste_vehicules
+
+
+
+
+
+
+
