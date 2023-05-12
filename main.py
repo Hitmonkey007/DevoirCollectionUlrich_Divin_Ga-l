e5219043ff8388109c65e17682d891689cabbb62
hitmonkey007 <igiranezaulrich19@gmail.com>
2023-05-12 08:39:05 +0200

    Fin de la Question II

--- main.py
+++ main.py
@@ -3,23 +3,23 @@
 ##__Creation de la liste__##
 liste_vehicules = ['1. Kawasaki', '2. Bugati','3. Toyota','4. Subaru','5. BMW','6. Fiat','7. Chevrolet','8. Mercedes','9. Yamaha','10. Hyundai']
 print(' Question I  :')
-print('--------------')
+print('------------\n')
 #--1-- Affichage des elements de la liste--#
-print("--1: La liste des Vehicules:")
-print('___________________________')
+print("--1--: La liste des Vehicules:")
+print('____________________________\n')
 for element in liste_vehicules:
     print(element)
 
 #--2-- Changer l'element numero 5--#
-print("--2: Liste apres le changement du numero 5:")
-print('___________________________________________')
+print("--2--: Liste apres le changement du numero 5:")
+print('___________________________________________\n')
 liste_vehicules.insert (5,'5. Lamborghini')
 for element in liste_vehicules:
     print(element)
 
 #--3-- Creer une nouvelle liste en remplissant avec les elements precedent--#
-print("--3: NOuvelle liste:")
-print('____________________')
+print("--3--: NOuvelle liste:")
+print('____________________\n')
 liste_new = []
 for elt in liste_vehicules:
     if 'a' in elt:
@@ -28,60 +28,96 @@ for i in liste_new:
     print(i)
 
 #--4-- Ajouter un element a la fin de la liste--#
-print('--4: Ajout element a la fin de la liste:')
-print('________________________________________')
+print('--4--: Ajout element a la fin de la liste:')
+print('________________________________________\n')
 liste_vehicules.insert(11,'11. Ferrari') 
 for i in liste_vehicules:
     print(i)
 
 #--5-- Ajouter un element a l'index numero 2--#
-print('--5: Ajout_element a l_index numero 2: ')
-print('_______________________________________')
+print('--5--: Ajout_element a l_index numero 2: ')
+print('_______________________________________\n')
 liste_vehicules.insert(2,'3. Jaguar')
 for j in liste_vehicules:
     print(j)
 #--6-- Supprimer l'élément numéro 3--#
-print('--6: La suppression du vehicule numero 3 :')
-print('__________________________________________')
+print('--6--: La suppression du vehicule numero 3 :')
+print('__________________________________________\n')
 del liste_vehicules[3]
 for i in liste_vehicules:
     print(i)
 
 #--7-- Supprimer l'élément à l’index numéro 2--#
-print('--7: La suppresion du vehicule a l_index numero 2:')
-print('__________________________________________________') 
+print('--7--: La suppresion du vehicule a l_index numero 2:')
+print('__________________________________________________\n') 
 del liste_vehicules[2]
 for i in liste_vehicules:
     print(i)
 #--8--  Ordonner la liste --#
-print('--8: Liste ordonnee:')
-print('___________________') 
+print('--8--: Liste ordonnee:  ')
+print('______________________\n') 
 liste_vehicules.sort()
 for i in liste_vehicules:
     print(i)
 
 #--9-- Afficher la liste au sens inverse --#
-print('--9: Liste affichee en sens inverse:')
-print('____________________________________') 
+print('--9--: Liste affichee en sens inverse:  ')
+print('______________________________________\n') 
 liste_vehicules.reverse()
 for i in liste_vehicules:
     print(i)
 
 #--10-- Vider la liste --#
-print('--10: Vider la liste:')
-print('_____________________')
+print('--10--: la liste est videe  ')
+print('__________________________\n')
 liste_vehicules.clear()
 for j in liste_vehicules:
     print(j)
 
 #--11-- Supprimer la liste --# 
-print('--11: La liste a ete supprimee')
-print('______________________________')
+print('--11--: La liste a ete supprimee  ')
+print('________________________________\n')
 del liste_vehicules
 
+#--Q.2--#
+print(' Question II  :')
+print('------------\n')
+#--1--Afficher le nombre de fois le chiffre que 3 apparait--#
+tupl = (1,2,3,3,4,5,3,7,8,9)
+nbre_fois_3 = 0
+check_3 =tupl[0]
+print('les elements du tuple sont :',tupl)
+for i in range(len(tupl)):
+    chiffre_3 =tupl[i]
+    if chiffre_3 == 3:
+        nbre_fois_3 +=1
+print("#--1-- voici le nombre de fois qu'apparait le chiffre 3 :",nbre_fois_3,"fois\n")
 
+#--2--Afficher le contenu de l'element numero 5--#
 
+print("#--2-- voici l'element ",tupl[5])
+print('\n')
+#--3-- ordonner la tuple--#
 
+ordonn = sorted(tupl)
+print('#--3-- voici le tuplet ordonné :', ordonn)
+print('\n')
+
+#--4--Ajouter un element a la fin de la ligne --#
+print("#--4-- voici l'element ajouter a la fin du tuplet :")
+tupl = tupl + (4,)
+print(tupl)
+print('\n')
+#----5----Ajouter un element a l'index 3 ------------------------ :
+
+print('#--5-- voici la liste apres ajout d_un element a l_index numero 3:')
+tupl = tupl[:3] +(50,)
+print('\n')
+
+#----6----affiche la nouvelle tuplet apres l'ajout ------------------------ :
+print('#--6-- la nouvelle tuple apres l_ajout')
+print(tupl)
+print('\n')
 
 
 
